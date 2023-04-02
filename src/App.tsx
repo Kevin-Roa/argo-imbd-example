7c97db61dafeefcea7d2be8ed91f007a5f06da4a
Aliah Shaira De Guzman <asndeguzman@gmail.com>
2023-04-02 10:57:05 -0500

    Base implementation of UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from '@utd-argo/ux-master-library';
+import MovieList from './components/MovieList/MovieList';
+import ReviewForm from './components/ReviewForm/ReviewForm';
+import NewsPanel from './components/NewsPanel/NewsPanel';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+	<Box sx={{  backgroundColor: '#ffffff', color: '#000000'}}>
+		<Grid container spacing={2}>
+		    <Grid item xs={12}>
+				<Header variant="global" title="Fresh Oranges" backgroundColor="#fc9f00" />
+			</Grid>
+			<Grid container direction="row" alignItems="stretch">
+				<Grid item xs={7}>
+					<MovieList />
+				</Grid>
+				<Grid direction="column" sx={{ paddingLeft: '40px'}}>
+				<Grid item xs={5}>
+					<ReviewForm />
+				</Grid>
+				<Grid item xs={5}>
+					<NewsPanel />
+				</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	</Box>
+
   )
 }
 
---
+++ src/components/MovieList/MovieInfo.tsx
@@ -0,0 +1,20 @@
+export const MovieInfo = [
+	{"title": "Everything Everywhere All At Once", 
+		"directors": "Daniel Kwan, Daniel Scheinert",
+		"desc": "A middle-aged Chinese immigrant is swept up into an insane adventure in which she alone can save existence by exploring other universes and connecting with the lives she could have led.",
+		"score": 7.9},
+	{"title": "The Banshees of Inisherin", 
+		"directors": "Martin McDonagh",
+		"desc": "Two lifelong friends find themselves at an impasse when one abruptly ends their relationship, with alarming consequences for both of them.",
+		"score": 7.7},
+	{"title": "The Fabelmans", 
+		"directors": "Steven Spielberg",
+		"desc": "Growing up in post-World War II era Arizona, young Sammy Fabelman aspires to become a filmmaker as he reaches adolescence, but soon discovers a shattering family secret and explores how the power of films can help him see the truth.",
+		"score": 7.6},
+	{"title": "Elvis", 
+		"directors": "Baz Luhrmann",
+		"desc": "The life of American music icon Elvis Presley, from his childhood to becoming a rock and movie star in the 1950s while maintaining a complex relationship with his manager, Colonel Tom Parker.",
+		"score": 7.3}
+]
+
+export default MovieInfo
