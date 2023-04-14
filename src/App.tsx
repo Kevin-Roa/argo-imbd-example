2bb085d4ded923c57486fac93e0c3a9b14ea45ef
tewashi <teavwashington@gmail.com>
2023-04-14 15:06:08 -0500

    added global header with tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@utd-argo/ux-master-library';
+import { Header, CustomTab } from '@utd-argo/ux-master-library';
 import MovieList from './components/MovieList/MovieList';
 import ReviewForm from './components/ReviewForm/ReviewForm';
 import NewsPanel from './components/NewsPanel/NewsPanel';
@@ -9,20 +9,41 @@ import Typography from '@mui/material/Typography';
 import './App.css';
 
 function App() {
-
   return (
-		<div className="root">
-			<Header variant="global" title="Fresh Oranges" backgroundColor="#fc9f00" />
-			<div className="content">
-				<div className="movies">
-					<MovieList />
-				</div>
-				<div className="panel">
-					<ReviewForm />
-					<NewsPanel />
-				</div>
-			</div>
-		</div>
+    <div className="root">
+      <Header
+        variant="global-tabs"
+        title="Fresh Oranges"
+        backgroundColor="#fc9f00"
+        titleFontWeight="500"
+        titleColor="#ffffff"
+        menuOptions={[
+          ['Top Rated', 'Action', 'Comedy', 'Mystery'],
+          ['Popular', 'Fan Favorites', 'Upcoming Series', 'Episode Recaps'],
+          ['New Releases', 'Industry News', 'Awards', 'Behind the Scenes'],
+          ['Profile', 'My List', 'Settings & Privacy', 'Log Out']
+        ]}
+        customTab={
+          <CustomTab
+            labels={['MOVIES', 'TV SHOWS', 'NEWS', 'NAME']}
+            fontColor="#fff"
+            fontSize="18px"
+            hoverColor="#fff"
+            borderRadius="4px"
+            height="64px"
+          />
+        }
+      />
+      <div className="content">
+        <div className="movies">
+          <MovieList />
+        </div>
+        <div className="panel">
+          <ReviewForm />
+          <NewsPanel />
+        </div>
+      </div>
+    </div>
   )
 }
 
