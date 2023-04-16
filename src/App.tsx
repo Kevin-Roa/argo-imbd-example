84db0eeb411d5d09e0caa2ab3537dccb896d611b
Kevin Roa <kevinroadev@gmail.com>
2023-04-15 23:25:12 -0500

    Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
-import { Header } from '@utd-argo/ux-master-library';
+import { Header, LoginPage } from '@utd-argo/ux-master-library';
 import MovieList from './components/MovieList/MovieList';
 import ReviewForm from './components/ReviewForm/ReviewForm';
 import NewsPanel from './components/NewsPanel/NewsPanel';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import './App.css';
 
 function App() {
-
-  return (
+	return (
 		<div className="root">
-			<Header variant="global" title="Fresh Oranges" backgroundColor="#fc9f00" />
-			<div className="content">
-				<div className="movies">
-					<MovieList />
-				</div>
-				<div className="panel">
-					<ReviewForm />
-					<NewsPanel />
-				</div>
-			</div>
+			<LoginPage
+				logo={
+					<h1
+						style={{
+							color: 'orange',
+							fontWeight: 'bold',
+							fontSize: '2.5rem'
+						}}
+					>
+						Fresh Oranges
+					</h1>
+				}
+				bgColor1="#ff9500"
+				bgColor2="#ff4d00"
+				accentColor="orange"
+			>
+				<>
+					<Header
+						variant="global"
+						title="Fresh Oranges"
+						backgroundColor="#fc9f00"
+					/>
+					<div className="content">
+						<div className="movies">
+							<MovieList />
+						</div>
+						<div className="panel">
+							<ReviewForm />
+							<NewsPanel />
+						</div>
+					</div>
+				</>
+			</LoginPage>
 		</div>
-  )
+	);
 }
 
-export default App
+export default App;
